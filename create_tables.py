644010b5914ee2c9d9eb125e654aeaec1ba03a6d
Felipe Augusto Azuero Mutis <azuero@protonmail.com>
2021-11-06 13:54:28 +0100

    Added docstrings.

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,40 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drop each table using the queries in `drop_table_queries` list.
+
+    Takes a connection to a database and a cursor using that connection and 
+    executes all the queries in the `drop_table_queries` list.
+
+    Args:
+        conn (conection): A connection to the database
+        cur (cursor): A cursor using the `conn` connection
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create each table using the queries in `create_table_queries` list.
+
+    Takes a connection to a database and a cursor using that connection and 
+    executes all the queries in the `create_table_queries` list.
+
+    Args:
+        conn (conection): A connection to the database
+        cur (cursor): A cursor using the `conn` connection
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Creates a connection and a cursor using that connection to the database 
+    whose attributes are listed in the config file, and runs the drop_tables and
+    create_tables functions.
+    """
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
--- etl.py
+++ etl.py
@@ -4,18 +4,41 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """COPY into each staging table using the queries in `copy_table_queries`.
+
+    Takes a connection to a database and a cursor using that connection and 
+    executes all the queries in the `copy_table_queries` list.
+
+    Args:
+        conn (conection): A connection to the database
+        cur (cursor): A cursor using the `conn` connection
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """INSERT into each final table using the queries in `insert_table_queries`.
+
+    Takes a connection to a database and a cursor using that connection and 
+    executes all the queries in the `insert_table_queries` list.
+
+    Args:
+        conn (conection): A connection to the database
+        cur (cursor): A cursor using the `conn` connection
+    """
+    
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Creates a connection and a cursor using that connection to the database 
+    whose attributes are listed in the config file, and runs the 
+    load_staging_tables and insert_tables functions.
+    """
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
